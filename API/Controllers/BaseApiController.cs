e992ed6666cf3ed7760e09ad25fd2a9ca981dc04
Ante <ante.hellgren@inter.ikea.com>
2022-02-28 13:21:16 +0100

    Added CRUD and object mapper

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Controllers
 {
@@ -13,6 +14,7 @@ namespace API.Controllers
     {
         private IMediator _mediator;
 
-        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices
+            .GetService<IMediator>();
     }
 }
--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -16,13 +16,16 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
             private readonly DataContext _context;
+
             public Handler(DataContext context)
             {
                 _context = context;
             }
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return await _context.Activities.ToListAsync();
+
+    
+                return await _context.Activities.ToListAsync(cancellationToken);
             }
         }
     }
