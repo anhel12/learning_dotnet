5192935d45a666d1534dd50db42f04fd21d27211
Ante <ante.hellgren@inter.ikea.com>
2022-04-09 13:40:12 +0200

    Endpoints for comments

---
+++ Application/Comments/List.cs
@@ -0,0 +1,48 @@
+﻿using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Comments
+{
+    public class List
+    {
+        public class Query : IRequest<Result<List<CommentDto>>>
+        {
+            public Guid ActivityId { get; set; } 
+        }
+        public class Handler : IRequestHandler<Query, Result<List<CommentDto>>>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<Unit> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var comments = await _context.Comments
+                    .Where(x => x.Activity.Id == request.ActivityId)
+                    .OrderBy(x => x.CreatedAt)
+                    .ProjectTo<CommentDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync();
+
+                return Result<List<CommentDto>>.Success(comments);
+            }
+
+            Task<Result<List<CommentDto>>> IRequestHandler<Query, Result<List<CommentDto>>>.Handle(Query request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
